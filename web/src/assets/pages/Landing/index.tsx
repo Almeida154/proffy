325fecac15d311a35843cf9469bf72d7550e443e
Almeida154 <davidalmeida154of@gmail.com>
2022-07-05 21:15:17 -0300

    :sparkles: Landing Page

--- web/src/assets/pages/Landing/index.tsx
+++ web/src/assets/pages/Landing/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
 
+import './styles.css';
+
+import logoIMG from '../../images/logo.svg';
+import landingIMG from '../../images/landing.svg';
+
+import studyIC from '../../images/icons/study.svg';
+import giveClassesIC from '../../images/icons/give-classes.svg';
+import purpleHeartIC from '../../images/icons/purple-heart.svg';
+
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
+    <div id="page-landing">
+      <div className="container" id="page-landing-content">
+        <div className="logo-container">
+          <img src={logoIMG} alt="Proffy" />
+          <h2>Sua plataforma de estudos online</h2>
+        </div>
+
+        <img
+          src={landingIMG}
+          alt="Plataforma de estudos"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <a href="#" className="study">
+            <img src={studyIC} alt="Estudar" />
+            Estudar
+          </a>
+          <a href="#" className="give-classes">
+            <img src={giveClassesIC} alt="Dar aulas" />
+            Dar aulas
+          </a>
+        </div>
+
+        <span className="total-connections">
+          Total de 200 conexões já realizadas{' '}
+          <img src={purpleHeartIC} alt="Coração roxo" />
+        </span>
+      </div>
     </div>
   );
 };
